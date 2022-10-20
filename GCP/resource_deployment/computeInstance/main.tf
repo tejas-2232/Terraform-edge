69caa9432e7211ca51c4492b93794b3c42037efe
tejas-2232 <tejasbachhav98@gmail.com>
2022-10-20 17:06:13 +0530

    create vm instance using tf

---
+++ GCP/resource_deployment/computeInstance/main.tf
@@ -0,0 +1,17 @@
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+
+    }
+  }
+}
---
+++ GCP/resource_deployment/computeInstance/provider.tf
@@ -0,0 +1,7 @@
+provider "google" {
+
+  project     = "serene-flare-364810" # project id
+  region      = "us-central1"
+  zone        = "us-central1-a"
+
+}
